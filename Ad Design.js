e5ff1bba0f8b6bab076ae2b79ba3a308df2487df
Denis Menshikov <34773356+DenisMenshikov@users.noreply.github.com>
2019-06-18 10:43:52 -0400

    Create Ad Design.js

---
+++ Ad Design.js
@@ -0,0 +1,65 @@
+//setting background
+background(196, 237, 176);
+
+//title section
+fill(150, 204, 224);
+rect(0,0,399,44);
+
+//header
+fill(166, 3, 3);
+textSize(30);
+text("            Computers!!!", 10, 30);
+
+//subtext
+textSize(13);
+text("Cant live with them, cant live without them, cant live in them either.", 6, 63);
+
+//Monitor Title section
+fill(150, 204, 224);
+rect(70, 80, 260, 50);
+
+//Monitor Title
+textSize(29);
+fill(166, 3, 3);
+text("Very Thin Monitor!", 82, 120);
+
+//Monitor
+
+//strut
+fill(0, 0, 0);
+strokeWeight(10);
+line(200, 200, 200, 260);
+line(170, 265, 230, 265);
+
+//screen
+fill(110, 110, 110);
+strokeWeight(4);
+rect(104, 140, 192, 108);
+
+//text
+fill(0, 255, 17);
+textSize(9);
+text("You can type, you can program!", 108, 155);
+
+//pacman
+fill(233, 250, 0);
+noStroke();
+text("You can play!", 114, 177);
+arc(120, 191, 10, 10, 40, 320);
+ellipse(130, 191, 4, 4);
+ellipse(140, 191, 4, 4);
+ellipse(150, 191, 4, 4);
+ellipse(160, 191, 4, 4);
+ellipse(170, 191, 4, 4);
+
+//youtube
+fill(255, 0, 0);
+text("Watch Videos!", 211, 177);
+rect(208, 200, 69, 16);
+rect(219, 191, 43, 40);
+ellipse(220, 203, 25, 25);
+ellipse(265, 203, 25, 25);
+ellipse(220, 219, 25, 25);
+ellipse(265, 219, 25, 25);
+fill(255, 255, 255);
+triangle(234, 222, 234, 200, 256, 211);
